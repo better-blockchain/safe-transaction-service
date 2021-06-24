cabc086a7cf2a5bfe430f1962711e15658c98a07
hhstore <hhglory@outlook.com>
2021-06-24 14:06:35 +0800

    hotfix

---
+++ docker/web/celery/worker/run_group.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# DEBUG set in .env_docker_compose
+if [ ${DEBUG:-0} = 1 ]; then
+    log_level="debug"
+else
+    log_level="info"
+fi
+
+if [ ${DJANGO_SETTINGS_MODULE} = "config.settings.production" ]; then
+  export DJANGO_SETTINGS_MODULE="config.settings.production_celery"
+fi
+
+sleep 1  # Wait for migrations
+echo "==> $(date +%H:%M:%S) ==> Running Celery worker <=="
+echo "==> $(date +%H:%M:%S) ==> worke queue: ${CELERY_WATCH_QUEUE}"
+exec celery -A config.celery_app worker --loglevel $log_level --pool=gevent --autoscale=120,80 -Q ${CELERY_WATCH_QUEUE}
